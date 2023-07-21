a045eea735f5cde25dfdc6b12685bca56e2f1720
harinderjitsingh <harinderjitss@gmail.com>
2023-07-21 11:26:30 -0400

    7212023-4

--- terraform/azure/modules/mssqlfgmodule/locals.tf
+++
@@ -1,7 +0,0 @@
-locals {
-  rg_name = "${var.fg_name}-rg"
-}
-
-# locals {
-#   consumable = azurerm_sql_managed_instance_failover_group.mssqlfg
-# }
--- terraform/azure/modules/trafficmanagermodule/locals.tf
+++
@@ -1,7 +0,0 @@
-locals {
-  rg_name = "${var.name}-rg"
-}
-
-locals {
-  consumable = azurerm_traffic_manager_profile.this
-}
